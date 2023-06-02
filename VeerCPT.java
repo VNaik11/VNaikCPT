287230afe47105b6abfccbe37212065a3ee01e65
VNaik11 <veer.naik24@ycdsbk12.ca>
2023-06-02 11:45:24 -0400

    Starting the game.

--- VeerCPT.java
+++ VeerCPT.java
@@ -2,10 +2,13 @@ import arc.*;
 
 public class VeerCPT{
 	public static void main(String[] args){
-		Console con = new Console();
-		
-		//Tester
-		//dsfdsf
-		
+	Console con = new Console("Connect 4 - CPT",1280,720);
+	
+	//Create the main menu of the game.
+	
+	
+	
+	
+	
 	}
-}
+}	
