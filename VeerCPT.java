9ca1c27d7da26bff94f16aecb5f3e437810e1602
VNaik11 <veer.naik24@ycdsbk12.ca>
2023-06-02 12:46:47 -0400

    Added image.

--- VeerCPT.java
+++ VeerCPT.java
@@ -1,10 +1,15 @@
 import arc.*;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
 
 public class VeerCPT{
 	public static void main(String[] args){
 	Console con = new Console("Connect 4 - CPT",1280,720);
 	
 	//Create the main menu of the game.
+	
+	BufferedImage imgMain = con.loadImage("MainMenu.jpg");
+		con.drawImage(imgMain, 0, 0);
 	
 	
 	
