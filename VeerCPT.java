bf3ee6fe0c6bb0cd22677af836e0bb7b3597e0f6
VNaik11 <veer.naik24@ycdsbk12.ca>
2023-06-05 20:12:13 -0400

    Started FileIO, and Main Menu

--- VeerCPT.java
+++ VeerCPT.java
@@ -4,24 +4,36 @@ import java.awt.Color;
 
 public class VeerCPT{
 	public static void main(String[] args){
-<<<<<<< Updated upstream
 	Console con = new Console("Connect 4 - CPT",1280,720);
 	
 	//Create the main menu of the game.
 	
-	BufferedImage imgMain = con.loadImage("MainMenu.jpg");
-		con.drawImage(imgMain, 0, 0);
-	
-	
-	
-	
-	
-=======
-		Console con = new Console();
+	BufferedImage imgMainMenu = con.loadImage("MainMenu.jpg");
+		con.drawImage(imgMainMenu, 0, 0);
 		
-		//Tester
-		//dsfdsfff
-		
->>>>>>> Stashed changes
+		game(con);
 	}
+	//FileIO (Input) User Input to Track The Users Inputs
+	
+	public static void game(Console con){
+	
+	TextOutputFile txtRecording = new TextOutputFile("recording.txt", true);
+	int intMove;
+	con.println("Enter the move you would like to make.");
+	intMove = con.readInt();
+	if(intMove > 7){
+		con.println("Please enter a valid move.");
+	}else if(intMove < 0){
+		con.println("Please enter a valid move.");
+	}
+	txtRecording.println(intMove);
+	txtRecording.close();
 }
+	
+	//File IO (Output) Replay the Users Moves
+	
+	
+	
+
+	}
+
