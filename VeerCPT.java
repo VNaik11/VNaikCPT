7bbd0fddaeba155284a3a72ab20281aa7bc94307
VNaik11 <veer.naik24@ycdsbk12.ca>
2023-06-08 11:12:02 -0400

    Strarted making the game.

--- VeerCPT.java
+++ VeerCPT.java
@@ -11,11 +11,67 @@ public class VeerCPT{
 	BufferedImage imgMainMenu = con.loadImage("MainMenu.jpg");
 		con.drawImage(imgMainMenu, 0, 0);
 		
+	String strChoice;
+		
+	strChoice = con.readLine();
+	if(strChoice.equals("play")){
 		game(con);
+	}else if(strChoice.equals("record")){
+		//recording(con);
+	}else if(strChoice.equals("help")){
+		//help(con);
+	}else{
+		con.println("Error");
 	}
-	//FileIO (Input) User Input to Track The Users Inputs
+}
 	
 	public static void game(Console con){
+	
+	//Enter your Usernames
+	String strP1;
+	String strP2;
+	
+	con.println("PLAYER ONE - ENTER YOUR USERNAME");
+	strP1 = con.readLine();
+	con.println("PLAYER TWO- ENTER YOUR USERNAME");
+	strP2 = con.readLine();
+	
+	//Array
+	
+	int intColumn;
+	int intRow;
+	
+	//Variables for each piece.
+	
+	int intP1Piece;
+	int intP2Piece;
+	
+	int intBall1X;
+	int intBall1Y;
+	intBall1X = 200;
+	intBall1Y = 200;
+	con.setDrawColor(Color.YELLOW);
+	con.fillOval(intBall1X, 100, 25, 25);
+	
+	int intBall2X;
+	int intBall2Y;
+	intBall2X = 200;
+	intBall2Y = 200;
+	con.setDrawColor(Color.RED);
+	con.fillOval(intBall1X, 100, 25, 25);
+	
+	String strBoard[][];
+	strBoard = new String[6][5];
+	
+	for(intRow = 0; intRow <5; intRow++){
+		//Placing the piece into column 1.
+		//strBoard[intColumn][0] = 
+		//Placing the piece into column 2.
+		//strBoard[intColumn][1] = 
+		}
+	
+	
+	//Recording the game.
 	
 	TextOutputFile txtRecording = new TextOutputFile("recording.txt", true);
 	int intMove;
@@ -31,7 +87,6 @@ public class VeerCPT{
 }
 	
 	//File IO (Output) Replay the Users Moves
-	
 	
 	
 
